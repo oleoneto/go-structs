dcc7d465b9037caad65501bd9169819deb7d8600
Leo <oleoneto@users.noreply.github.com>
2022-10-25 23:29:17 -0400

    feat(validator): implements struct validator (#5)
    * chore(workflows): bump tag action
    
    * fix(parser): return fields for nested structs
    
    * feat: update struct parser and improve test coverage
    
    * feat(validators): add validator with default rules

---
+++ validators/validator_test.go
@@ -0,0 +1,479 @@
+package validators
+
+import (
+	"reflect"
+	"testing"
+)
+
+type Identifiable struct {
+	UUID string `json:"id" validate:"uuid"`
+}
+
+type Contact struct {
+	IsActive     bool     `json:"is_active"`
+	Emails       []string `json:"emails" db:"emails" validate:"email,min=1"`
+	PhoneNumbers []string `json:"phones"`
+}
+
+type Person struct {
+	Identifiable
+	Name    string  `json:"name" db:"name" validate:"min=2,max=8"`
+	Contact Contact `json:"contact"`
+}
+
+func Test_IsIn(t *testing.T) {
+	type args struct {
+		value          reflect.Value
+		acceptedValues []string
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{
+			name: "numeric - 1",
+			args: args{
+				value:          reflect.ValueOf(2),
+				acceptedValues: []string{"2", "3", "5", "7"},
+			},
+			want: true,
+		},
+		{
+			name: "numeric - 2",
+			args: args{
+				value:          reflect.ValueOf(7),
+				acceptedValues: []string{"2", "3", "5", "7"},
+			},
+			want: true,
+		},
+		{
+			name: "numeric - 3",
+			args: args{
+				value:          reflect.ValueOf(0),
+				acceptedValues: []string{"2", "3", "5", "7"},
+			},
+			want: false,
+		},
+		{
+			name: "numeric - 4",
+			args: args{
+				value:          reflect.ValueOf(0),
+				acceptedValues: []string{"2a", "B"},
+			},
+			want: false,
+		},
+		{
+			name: "numeric - 5",
+			args: args{
+				value:          reflect.ValueOf(10.2),
+				acceptedValues: []string{"2a", "B"},
+			},
+			want: false,
+		},
+		{
+			name: "numeric - 6",
+			args: args{
+				value:          reflect.ValueOf([]int{}),
+				acceptedValues: []string{"2", "B"},
+			},
+			want: false,
+		},
+		{
+			name: "string - 1",
+			args: args{
+				value:          reflect.ValueOf("GUEST"),
+				acceptedValues: []string{"ADMIN", "GUEST"},
+			},
+			want: true,
+		},
+		{
+			name: "string - 2",
+			args: args{
+				value:          reflect.ValueOf("WRITER"),
+				acceptedValues: []string{"ADMIN", "GUEST"},
+			},
+			want: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := IsIn(tt.args.value, tt.args.acceptedValues); got != tt.want {
+				t.Errorf("IsIn() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_IsUUID(t *testing.T) {
+	tests := []struct {
+		name string
+		arg  string
+		want bool
+	}{
+		{
+			name: "test - 1",
+			arg:  "",
+			want: false,
+		},
+		{
+			name: "test - 2",
+			arg:  "someone-is-cool",
+			want: false,
+		},
+		{
+			name: "test - 3",
+			arg:  " ",
+			want: false,
+		},
+		{
+			name: "test - 4",
+			arg:  "00000000-0000-0000-0000-000000000000",
+			want: true,
+		},
+		{
+			name: "test - 5",
+			arg:  "21f2fa1d-c662-4669-abba-095a2416f5b9",
+			want: true,
+		},
+		{
+			name: "test - 6",
+			arg:  "097e3981-55a2-4f83-a5dd-405e49a80314",
+			want: true,
+		},
+		{
+			name: "test - 7",
+			arg:  "2b852002-f19d-11ec-8ea0-0242ac120002",
+			want: true,
+		},
+		{
+			name: "test - 8",
+			arg:  "2g852002-f19d-11ec-8ea0-0242ac120002",
+			want: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := IsUUID(tt.arg); got != tt.want {
+				t.Errorf("IsUUID() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_Validate(t *testing.T) {
+	type Resource struct {
+		Currency         string   `json:"currency" validate:"currency"`
+		Price            float64  `json:"price" validate:"min=5,max=30"`
+		Group            int      `json:"group" validate:"in=1|3|5|7"`
+		Type             string   `json:"type" validate:"in=USED|NEW"`
+		Code             string   `json:"code" validate:"eq=5"`
+		Quantity         int      `json:"qty" validate:"min=1,max=3"`
+		Rating           float64  `json:"rating" validate:"in=0|1|2|4|5"`
+		Related          []string `json:"related" validate:"uuid"`
+		PublishedAt      string   `json:"published_at" validate:"datetime"`
+		PriceAvailableIn []string `json:"price_available_in" validate:"currency"`
+	}
+
+	type S1 struct {
+		C []int `json:"c" validate:"currency"`
+		D []int `json:"d" validate:"datetime"`
+		E []int `json:"e" validate:"email"`
+		I []int `json:"i" validate:"in=1|2|3"`
+		U []int `json:"u" validate:"uuid"`
+	}
+
+	type S2 struct {
+		C *string `json:"c" validate:"currency"`
+		F string  `json:"f" validate:"min="`
+		G *string `json:"g" validate:"uuid"`
+		H *string `json:"h" validate:"in=1|2"`
+		K *string `json:"k" validate:"email"`
+		L *string `json:"l" validate:"datetime"`
+		M *int    `json:"m" validate:"min=3"`
+	}
+
+	tests := []struct {
+		name    string
+		model   any
+		options ValidationOptions
+		want    map[string][]string
+	}{
+		{
+			name:    "person - 1",
+			model:   Person{},
+			options: ValidationOptions{},
+			want: map[string][]string{
+				"id":             {"INVALID_FORMAT"},
+				"name":           {"INVALID_LENGTH"},
+				"contact.emails": {"INVALID_LENGTH"},
+			},
+		},
+		{
+			name: "person - 2",
+			model: Person{
+				Identifiable: Identifiable{UUID: ""},
+			},
+			options: ValidationOptions{},
+			want: map[string][]string{
+				"id":             {"INVALID_FORMAT"},
+				"name":           {"INVALID_LENGTH"},
+				"contact.emails": {"INVALID_LENGTH"},
+			},
+		},
+		{
+			name: "person - 3",
+			model: Person{
+				Identifiable: Identifiable{UUID: "2b852002-f19d-11ec-8ea0-0242ac120002"},
+			},
+			options: ValidationOptions{},
+			want: map[string][]string{
+				"name":           {"INVALID_LENGTH"},
+				"contact.emails": {"INVALID_LENGTH"},
+			},
+		},
+		{
+			name: "person - 4",
+			model: Person{
+				Identifiable: Identifiable{UUID: "2b852002-f19d-11ec-8ea0-0242ac120002"},
+				Name:         "Leonardo",
+			},
+			options: ValidationOptions{},
+			want: map[string][]string{
+				"contact.emails": {"INVALID_LENGTH"},
+			},
+		},
+		{
+			name: "person - 5",
+			model: Person{
+				Identifiable: Identifiable{UUID: "2b852002-f19d-11ec-8ea0-0242ac120002"},
+				Name:         "Leonardo",
+				Contact:      Contact{},
+			},
+			options: ValidationOptions{},
+			want: map[string][]string{
+				"contact.emails": {"INVALID_LENGTH"},
+			},
+		},
+		{
+			name: "person - 6",
+			model: Person{
+				Identifiable: Identifiable{UUID: "2b852002-f19d-11ec-8ea0-0242ac120002"},
+				Name:         "Leonardo",
+				Contact:      Contact{Emails: []string{"email"}},
+			},
+			options: ValidationOptions{},
+			want: map[string][]string{
+				"contact.emails[0]": {"INVALID_FORMAT"},
+			},
+		},
+		{
+			name: "person - 7",
+			model: Person{
+				Identifiable: Identifiable{UUID: "2b852002-f19d-11ec-8ea0-0242ac120002"},
+				Name:         "Leonardo",
+				Contact:      Contact{Emails: []string{"email@example.com"}},
+			},
+			options: ValidationOptions{},
+			want:    map[string][]string{},
+		},
+		{
+			name: "person - 8",
+			model: Person{
+				Identifiable: Identifiable{UUID: "2b852002-f19d-11ec-8ea0-0242ac120002"},
+				Name:         "Leonardo Ribeiro",
+				Contact:      Contact{Emails: []string{"email@example.com"}},
+			},
+			options: ValidationOptions{},
+			want: map[string][]string{
+				"name": {"INVALID_LENGTH"},
+			},
+		},
+		{
+			name: "resource - 1",
+			model: Resource{
+				Currency:    "BRL",
+				Price:       14,
+				Group:       7,
+				Type:        "NEW",
+				Code:        "ABC12",
+				Quantity:    2,
+				Rating:      5,
+				PublishedAt: "2020-01-01T00:00:00+01:00",
+			},
+			options: ValidationOptions{},
+			want:    map[string][]string{},
+		},
+		{
+			name: "resource - 2",
+			model: Resource{
+				Currency:         "BRL",
+				Price:            42,
+				Group:            42,
+				Type:             "AWESOME",
+				Code:             "ABC12",
+				Quantity:         2,
+				Rating:           42,
+				Related:          []string{"abc"},
+				PriceAvailableIn: []string{"AUD", "EURO"},
+			},
+			options: ValidationOptions{},
+			want: map[string][]string{
+				"price":                 {"INVALID_VALUE"},
+				"group":                 {"INVALID_VALUE"},
+				"type":                  {"INVALID_VALUE"},
+				"rating":                {"INVALID_VALUE"},
+				"related[0]":            {"INVALID_FORMAT"},
+				"published_at":          {"INVALID_FORMAT"},
+				"price_available_in[1]": {"INVALID_VALUE"},
+			},
+		},
+		{
+			name: "resource - 3",
+			model: Resource{
+				Currency:         "BRL",
+				Price:            42,
+				Group:            42,
+				Type:             "AWESOME",
+				Code:             "ABC12",
+				Quantity:         2,
+				Rating:           42,
+				Related:          []string{"abc"},
+				PriceAvailableIn: []string{"AUD", "EURO"},
+				PublishedAt:      "2022-01-01",
+			},
+			options: ValidationOptions{},
+			want: map[string][]string{
+				"price":                 {"INVALID_VALUE"},
+				"group":                 {"INVALID_VALUE"},
+				"type":                  {"INVALID_VALUE"},
+				"rating":                {"INVALID_VALUE"},
+				"related[0]":            {"INVALID_FORMAT"},
+				"published_at":          {"INVALID_FORMAT"},
+				"price_available_in[1]": {"INVALID_VALUE"},
+			},
+		},
+		{
+			name:    "s1 - 1",
+			model:   S1{C: []int{42}, D: []int{42}, E: []int{42}, I: []int{42}, U: []int{42}},
+			options: ValidationOptions{},
+			want: map[string][]string{
+				"c[0]": {"INVALID_DATA_TYPE"},
+				"d[0]": {"INVALID_DATA_TYPE"},
+				"e[0]": {"INVALID_DATA_TYPE"},
+				"i[0]": {"INVALID_VALUE"},
+				"u[0]": {"INVALID_DATA_TYPE"},
+			},
+		},
+		{
+			name:  "s2 - 1",
+			model: S2{},
+			options: ValidationOptions{
+				SkipRules: []string{"currency", "uuid", "datetime", "in"},
+			},
+			want: map[string][]string{
+				"f": {"INVALID_VALUE"},
+				"k": {"INVALID_FORMAT"},
+				"m": {"INVALID_VALUE"},
+			},
+		},
+		{
+			name:    "s2 - 2",
+			model:   S2{},
+			options: ValidationOptions{},
+			want: map[string][]string{
+				"c": {"INVALID_VALUE"},
+				"f": {"INVALID_VALUE"},
+				"g": {"INVALID_FORMAT"},
+				"h": {"INVALID_VALUE"},
+				"k": {"INVALID_FORMAT"},
+				"l": {"INVALID_FORMAT"},
+				"m": {"INVALID_VALUE"},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := Validate(tt.model, tt.options); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Validate() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_PassesRegex(t *testing.T) {
+	type args struct {
+		pattern string
+		str     string
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{
+			name: "numeric - 0",
+			want: false,
+			args: args{
+				pattern: `\d+`,
+				str:     "leonardo",
+			},
+		},
+		{
+			name: "numeric - 1",
+			want: true,
+			args: args{
+				pattern: `\d+`,
+				str:     "299",
+			},
+		},
+		{
+			name: "numeric - 2",
+			want: true,
+			args: args{
+				pattern: `\d{3}$`,
+				str:     "299",
+			},
+		},
+		{
+			name: "numeric - 3",
+			want: false,
+			args: args{
+				pattern: `^\d{2}$`,
+				str:     "299",
+			},
+		},
+		{
+			name: "US phone number - 1",
+			want: false,
+			args: args{
+				pattern: `^\d{3}-\d{3}-\d{4}$`,
+				str:     "55-5-5555-5555",
+			},
+		},
+		{
+			name: "US phone number - 2",
+			want: true,
+			args: args{
+				pattern: `^\d{3}-\d{3}-\d{4}$`,
+				str:     "555-555-5555",
+			},
+		},
+		{
+			name: "invalid - 1",
+			want: false,
+			args: args{
+				pattern: `/\`,
+				str:     "#FBA",
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := PassesRegex(tt.args.pattern, tt.args.str); got != tt.want {
+				t.Errorf("PassesRegex() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
